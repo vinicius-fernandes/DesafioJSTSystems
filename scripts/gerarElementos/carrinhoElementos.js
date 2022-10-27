f84267a718b0c088e604665420bce497fa21340f
f0fp1422 <viniciusnandes@hotmail.com>
2022-10-27 11:28:58 -0300

    Input total carrinho

--- scripts/gerarElementos/carrinhoElementos.js
+++ scripts/gerarElementos/carrinhoElementos.js
@@ -5,7 +5,13 @@ function gerarCardProdutoCarrinho(produtoCarrinho){
     <div class="col-12 w-100  m-1 bg-white border rounded p-2">
 
         <h5 class="card-title">${produtoCarrinho.descricao}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">Total no carrinho : ${produtoCarrinho.total}</h6>
+        <h6 class="card-subtitle mb-2 text-muted">Total no carrinho :</h6>
+        <div class="d-inline-flex ">
+        <button class="btn btn-dark btn-sm rounded-circle m-1"><i class="bi bi-arrow-down"></i></button>
+        <input type="number" class="col-3  id="exampleFormControlInput1" value="${produtoCarrinho.total}">
+       
+        <button class="btn btn-dark btn-sm rounded-circle m-1"><i class="bi bi-arrow-up"></i></button>
+        </div>
         <p class="card-text">Valor unitário: R$ ${produtoCarrinho.valor} | Valor total: R$ ${(produtoCarrinho.total)*(produtoCarrinho.valor)}</p>
         <a href="#" class="btn btn-danger" onclick="removerProdutoCarrinho(${produtoCarrinho.id})"><i class="bi bi-trash"></i></a>
  
